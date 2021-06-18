03f740a5c0e74a93d7f4df3e2a2f97a9b08a7ebb
Christopher <sayegh.topher@gmail.com>
2021-06-18 17:57:47 -0500

    testing api

---
+++ body.js
@@ -0,0 +1,6 @@
+{
+    "coding": "8",
+    "from": "SMSInfo",
+    "hex-content": "00480065006c006c006f",
+    "to": 9137446456
+}
---
+++ schema.js
@@ -0,0 +1,17 @@
+{2 items
+    "properties":{4 items
+    "coding":{1 item
+    "type":"string"
+    }
+    "from":{1 item
+    "type":"string"
+    }
+    "hex-content":{1 item
+    "type":"string"
+    }
+    "to":{1 item
+    "type":"integer"
+    }
+    }
+    "type":"object"
+    }

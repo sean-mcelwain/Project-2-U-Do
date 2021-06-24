c1e0dd5775cf74591efdd6177a39e86a575cfc6f
sean-mcelwain <79949547+sean-mcelwain@users.noreply.github.com>
2021-06-24 16:49:55 -0500

    tried to get functionalist working, not sure why not

---
+++ public/js/list.js
@@ -0,0 +1,46 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#list-name').value.trim();
+  const description = document.querySelector('#list-desc').value.trim();
+
+  if (name && description) {
+    const response = await fetch(`/api/lists`, {
+      method: 'POST',
+      body: JSON.stringify({ name, description }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to create list');
+    }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/lists/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete list');
+    }
+  }
+};
+
+document
+  .querySelector('.new-list-form')
+  .addEventListener('submit', newFormHandler);
+
+document
+  .querySelector('.list-list')
+  .addEventListener('click', delButtonHandler);

cb453efd7ec2db189d7e23734155ad73751a68c8
Christopher <sayegh.topher@gmail.com>
2021-06-24 20:31:04 -0500

    added ENV and updated sms api

--- apistuff/sms2.js
+++ apistuff/sms2.js
@@ -1,15 +1,28 @@
+require("dotenv").config()
 var request = require('request');
+var body = { messages: [
+  {
+    to: [
+      "+19137446456",
+      "+16198470281"
+    ],
+    content: "To-do list app reminder",
+    from: "D7sms"
+  }]};
+
 var options = {
   'method': 'POST',
-  'url': 'https://rest-api.d7networks.com/secure/send',
+  'url': 'https://rest-api.d7networks.com/secure/sendbatch',
   'headers': {
     'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': 'Basic dnRiZzkxOTI6eWdleGxXVXU='
+    'Authorization': `Basic ${process.env.D7_KEY}`
   },
-  body: "{\n\t\"to\":\"+19137446456\",\n\t\"content\":\"Welcome to D7 sms , we will help you to talk with your customer effectively\",\n\t\"from\":\"SMSINFO\",\n\t\"dlr\":\"yes\",\n\t\"dlr-method\":\"GET\", \n\t\"dlr-level\":\"2\", \n\t\"dlr-url\":\"http://yourcustompostbackurl.com\"\n}"
+body:JSON.stringify(body)
 
 };
 request(options, function (error, response) {
   if (error) throw new Error(error);
   console.log(response.body);
 });
+
+console.log(options);

81e8f852b31bd6b9b1ab9092efd7e0ea1c134fa5
Christi <mahomes4president@gmail.com>
2021-07-04 14:33:16 -0500

    updating task/list routes

--- controllers/api/taskRoutes.js
+++ controllers/api/taskRoutes.js
@@ -4,18 +4,19 @@ const withAuth = require('../../utils/auth');
 
  router.post('/', withAuth, async (req, res) => {
      console.log("cats")
+console.log(req.body)
+const taskDesc = req.body.task
+const listId = req.body.listId
 
-//      try {
-  
-//       const newList = await List.create({
-//         ...req.body,
-//         user_id: req.session.user_id,
-//       });
-  
-//       res.status(200).json(newList);
-//     } catch (err) {
-//       res.status(400).json(err);
-//     }
+     try {
+      const newTask = await Task.create({
+description: taskDesc,
+        list_id: listId
+      });
+      res.status(200).json(newTask);
+    } catch (err) {
+      res.status(400).json(err);
+    }
   });
 
 
--- public/js/listUpdate.js
+++ public/js/listUpdate.js
@@ -25,25 +25,30 @@ const updateFormHandler = async (event) => {
 const addTask = async (event) => {
   event.preventDefault();
   console.log("jeans")
-
+  const listId = document.querySelector('.updateBtn').getAttribute('data-id');
   const newTask = document.querySelector('#new-task').value.trim();
-  console.log(newTask)
-
-//   if (name && description) {
-//     const response = await fetch(`/api/lists/${id}`, {
-//       method: 'PUT',
-//       body: JSON.stringify({ name, description }),
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//     });
-
+  
+  if (newTask) {
+    console.log(newTask)
+    console.log(listId)
+const  taskObj = {
+  task: newTask,
+  listId
+}
+    const response = await fetch(`/api/tasks/`, {
+      method: 'POST',
+       body: JSON.stringify( taskObj ),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+console.log(response)
 //     if (response.ok) {
 //       document.location.replace('/');
 //     } else {
 //       alert('Failed to update list');
 //     }
-//   }
+   }
  };
 document
 .querySelector('#update-list-form')
